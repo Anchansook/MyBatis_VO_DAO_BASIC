1d75825c8d329a2ad0d2d70fd842a929109bb2b0
ckstnr99 <ckstnr99@naver.com>
2024-12-20 11:05:17 +0900

    2024.12.20. 11:04 reaning project created

---
+++ src/main/java/com/learning/cookiestore/CookiestoreApplication.java
@@ -0,0 +1,13 @@
+package com.learning.cookiestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookiestoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookiestoreApplication.class, args);
+	}
+
+}
