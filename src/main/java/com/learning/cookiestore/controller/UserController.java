8e3d91a25fe6ffa159653cc02f7bb6cfb495eba6
ckstnr99 <ckstnr99@naver.com>
2024-12-23 14:03:07 +0900

    2024.12.23. 14:02 UserController - idCheck

---
+++ src/main/java/com/learning/cookiestore/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.learning.cookiestore.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.learning.cookiestore.exception.ResponseDto;
+import com.learning.cookiestore.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
+public class UserController {
+	private final UserService userService;
+
+	// 회원가입 - 아이디 중복 확인
+	@PostMapping("/id-check")
+	public ResponseDto idCheck(
+		@RequestBody String userId // @Valid는 불필요
+	) {
+		ResponseDto response = userService.idCheck(userId);
+		return response;
+	}
+	
+}
